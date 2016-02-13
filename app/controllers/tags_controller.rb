542df467724131199f193019f05973e1511cd263
NoneOfMaster <rcraigt@gmail.com>
2016-02-12 17:27:11 -0800

    specs pass, broken

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,10 +6,33 @@ class TagsController < ApplicationController
     @tags = Tag.all
   end
 
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+
+    respond_to do |format|
+      if @tag.save
+        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
+        format.json { render :show, status: :created, location: @tag }
+      else
+        format.html { render :new }
+        format.json { render json: @tag.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   # GET /tags/1
   # GET /tags/1.json
   def show
     @tag = Tag.find(params[:id])
   end
+
+  private
+    def tag_params
+      params[:tag].permit(:name)
+    end
 
 end
